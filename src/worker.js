dd629c1aa210b635f4482881d3e2c9b9fe3c7ed1
dio <doibncm2003@gmail.com>
2025-07-07 11:21:49 +0700

    Add Cloudflare Worker config

---
+++ src/worker.js
@@ -0,0 +1,14 @@
+export default {
+    async fetch(request) {
+      const url = new URL(request.url);
+      const path = url.pathname;
+  
+      const githubUser = "doi2523";
+      const repo = "cdn-locketdio";
+      const branch = "main";
+  
+      const targetUrl = `https://cdn.jsdelivr.net/gh/${githubUser}/${repo}@${branch}${path}`;
+      return fetch(targetUrl);
+    },
+  };
+  
